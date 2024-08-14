644ac32dfde4a3fde23b92ab31c460631648751f
astro-friedel <friedel@illinois.edu>
2024-08-14 13:05:09 -0500

    added patching to pthreadpool files

--- cmake/pthreadpool.cmake
+++ cmake/pthreadpool.cmake
@@ -10,22 +10,7 @@ else()
             URL_HASH SHA256=a4cf06de57bfdf8d7b537c61f1c3071bce74e57524fe053e0bbd2332feca7f95
             PREFIX "${CMAKE_BINARY_DIR}/${EPA}"
             DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+            PATCH_COMMAND patch -p0 -N --input=${CMAKE_SOURCE_DIR}/cmake/pthreadpool/pthreadpool.patch
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DPTHREADPOOL_BUILD_TESTS=OFF -DPTHREADPOOL_BUILD_BENCHMARKS=OFF -DPTHREADPOOL_ALLOW_DEPRECATED_API=OFF
-    )
-    include(CMakePackageConfigHelpers)
-    write_basic_package_version_file(
-            "${CMAKE_BINARY_DIR}/${EPA}/src/${EPA}-build/pthreadpoolConfigVersion.cmake"
-            VERSION 1.0.0
-            COMPATIBILITY AnyNewerVersion
-    )
-
-    configure_file(
-            cmake/pthreadpool/pthreadpoolConfig.cmake.in
-            "${CMAKE_BINARY_DIR}/${EPA}/src/${EPA}-build/pthreadpoolConfig.cmake"
-            @ONLY
-    )
-    install(
-            FILES "${CMAKE_BINARY_DIR}/${EPA}/src/${EPA}-build/pthreadpoolConfigVersion.cmake" "${CMAKE_BINARY_DIR}/${EPA}/src/${EPA}-build/pthreadpoolConfig.cmake"
-            DESTINATION lib/cmake/pthreadpool
     )
 endif()
