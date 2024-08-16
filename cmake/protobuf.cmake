a39eef6b0d021b31eb630265215333a0e1e8f4f6
astro-friedel <friedel@illinois.edu>
2024-08-16 10:14:19 -0500

    added protobuf to build

---
+++ cmake/protobuf.cmake
@@ -0,0 +1,18 @@
+find_package(protobuf 3.19 QUIET CONFIG)
+if(protobuf_FOUND)
+    report_found(protobuf "${protobuf_VERSION}")
+else()
+    set(EPA protobuf)
+    report_build(${EPA})
+    ExternalProject_Add(
+            ${EPA}
+            GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
+            GIT_TAG v3.19.1
+            GIT_PROGRESS TRUE
+            PREFIX "${CMAKE_BINARY_DIR}/${EPA}"
+            DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            SOURCE_SUBDIR cmake
+            GIT_SUBMODULES_RECURSE TRUE
+    )
+endif()
