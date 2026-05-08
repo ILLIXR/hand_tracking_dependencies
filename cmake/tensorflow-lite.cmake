5742983cb11a1d1a8d04238c37bf64b876584f28
astro-friedel <friedel@illinois.edu>
2026-05-08 16:21:09 -0500

    better handling of tensorflow dual installs

--- cmake/tensorflow-lite.cmake
+++ cmake/tensorflow-lite.cmake
@@ -2,13 +2,14 @@ set(TFLIBRARY_POSTFIX ${LIBRARY_POSTFIX} CACHE STRING "" FORCE)
 if(ENABLE_GPU)
     set(TFLIBRARY_POSTFIX "${TFLIBRARY_POSTFIX}-gpu" CACHE STRING "" FORCE)
 endif()
-find_package(tensorflow-lite${TFLIBRARY_POSTFIX} QUIET CONFIG)
-if(tensorflow-lite${TFLIBRARY_POSTFIX}_FOUND)
+set(tfl_name tensorflow-lite${TFLIBRARY_POSTFIX})
+find_package(${tfl_name} QUIET CONFIG)
+if(${tfl_name}_FOUND)
     report_found(tensorflow-lite "")
 else()
-    fetch_git(NAME tensorflow-lite${TFLIBRARY_POSTFIX}
+    fetch_git(NAME ${tfl_name}
               REPO https://github.com/ILLIXR/tensorflow-lite
-              TAG cdef8ca128586de113858b81421b0ceea4c72bb0
+              TAG 9555e9c53b362c785fc6da5583f1e04522965fdf
               NO_OVERRIDE
     )
     set(TFLITE_ENABLE_INSTALL ON CACHE BOOL "" FORCE)
@@ -17,7 +18,7 @@ else()
     set(TFLITE_ENABLE_RUY ON CACHE BOOL "" FORCE)
     set(TFLITE_ENABLE_NNAPI ON CACHE BOOL "" FORCE)
 
-    configure_target(NAME tensorflow-lite${TFLIBRARY_POSTFIX}
+    configure_target(NAME ${tfl_name}
                      NO_FIND
     )
     unset(TFLITE_ENABLE_INSTALL CACHE)
@@ -25,6 +26,9 @@ else()
     unset(TFLITE_ENABLE_GPU CACHE)
     unset(TFLITE_ENABLE_RUY CACHE)
     unset(TFLITE_ENABLE_NNAPI CACHE)
-    set(tensorflow-lite${TFLIBRARY_POSTFIX}_DIR ${tensorflow-lite${TFLIBRARY_POSTFIX}_BINARY_DIR} CACHE PATH "" FORCE)
-    set(tensorflow-lite${TFLIBRARY_POSTFIX}_SOURCE_DIR ${tensorflow-lite_SOURCE_DIR} CACHE PATH "" FORCE)
+    set(tensorflow-lite${TFLIBRARY_POSTFIX}_BINARY_DIR ${tensorflow-lite_BINARY_DIR} CACHE PATH "" FORCE)
+    set(${tfl_name}_DIR ${tensorflow-lite${TFLIBRARY_POSTFIX}_BINARY_DIR} CACHE PATH "" FORCE)
+    set(${tfl_name}_SOURCE_DIR ${tensorflow-lite_SOURCE_DIR} CACHE} CACHE PATH "" FORCE)
+    unset(tensorflow-lite_BINARY_DIR CACHE)
+    unset(tensorflow-lite_SOURCE_DIR CACHE)
 endif()
