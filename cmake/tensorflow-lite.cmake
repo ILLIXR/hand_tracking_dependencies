3a5fbb4baafff03d36ea631380896a4c55b75c2a
astro-friedel <friedel@illinois.edu>
2026-05-08 15:15:23 -0500

    fix for incorrect path

--- cmake/tensorflow-lite.cmake
+++ cmake/tensorflow-lite.cmake
@@ -6,9 +6,10 @@ find_package(tensorflow-lite${TFLIBRARY_POSTFIX} QUIET CONFIG)
 if(tensorflow-lite${TFLIBRARY_POSTFIX}_FOUND)
     report_found(tensorflow-lite "")
 else()
-    fetch_git(NAME tensorflow-lite
+    fetch_git(NAME tensorflow-lite${TFLIBRARY_POSTFIX}
               REPO https://github.com/ILLIXR/tensorflow-lite
               TAG 892dc20d59a894ed72b55bc2be756e0989bdc657
+              NO_OVERRIDE
     )
     set(TFLITE_ENABLE_INSTALL ON CACHE BOOL "" FORCE)
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
@@ -16,12 +17,14 @@ else()
     set(TFLITE_ENABLE_RUY ON CACHE BOOL "" FORCE)
     set(TFLITE_ENABLE_NNAPI ON CACHE BOOL "" FORCE)
 
-    configure_target(NAME tensorflow-lite)
-
+    configure_target(NAME tensorflow-lite${TFLIBRARY_POSTFIX}
+                     NO_FIND
+    )
     unset(TFLITE_ENABLE_INSTALL CACHE)
     set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
     unset(TFLITE_ENABLE_GPU CACHE)
     unset(TFLITE_ENABLE_RUY CACHE)
     unset(TFLITE_ENABLE_NNAPI CACHE)
-
+    set(tensorflow-lite${TFLIBRARY_POSTFIX}_DIR ${tensorflow-lite${TFLIBRARY_POSTFIX}_BINARY_DIR} CACHE PATH "" FORCE)
+    set(tensorflow-lite${TFLIBRARY_POSTFIX}_SOURCE_DIR ${tensorflow-lite_SOURCE_DIR} CACHE PATH "" FORCE)
 endif()
